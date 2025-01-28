ba51c81e471f58e6e5d8f243b8f03be46c3aeb07
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-01-28 19:29:54 +0200

    013 add

---
+++ sql/013-query-1.sql
@@ -0,0 +1,14 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-01-28
+-- File: 013-query-1.sql
+
+SELECT  ename
+        , job
+        , hiredate
+        , empno
+FROM emp
+;
+
+
+
+-- End of file
