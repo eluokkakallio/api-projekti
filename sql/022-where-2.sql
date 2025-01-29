c9ab9b417233a1327f77907a787939081dafc4fa
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-01-29 10:02:29 +0200

    022 add

---
+++ sql/022-where-2.sql
@@ -0,0 +1,12 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-01-28
+-- File: 022-where-2.sql
+
+SELECT ename
+        , deptno
+FROM    emp
+WHERE empno = 7566
+;
+
+
+-- End of file
