69d8c3433a95673db7438536fea124fef0444d0b
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-01-29 09:55:23 +0200

    021 add

---
+++ sql/021-where-1.sql
@@ -0,0 +1,12 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-01-28
+-- File: 021-where-1.sql
+
+SELECT ename
+        , sal
+FROM emp
+WHERE sal > 2850
+;
+
+
+-- End of file
