9e4ddfc880586774ccd50acd3d7eccc4f58a872b
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-01-29 10:03:39 +0200

    025 add

---
+++ sql/025-where-5.sql
@@ -0,0 +1,12 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-01-28
+-- File: 025-where-5.sql
+
+SELECT ename
+        , deptno
+FROM emp
+WHERE deptno = 10 or deptno = 30
+;
+
+
+-- End of file
