70eafdc034138606fefc6b654c7ee7b49d4e3c32
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-02-27 23:14:56 +0200

    add 073

---
+++ sql/073-functions-3.sql
@@ -0,0 +1,18 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-02-27
+-- File: 073-functions-3
+
+SELECT empno
+        , ename
+        , COALESCE(sal, 0) * 1.15
+        AS "new salary"
+        , ROUND((COALESCE(sal, 0) * 1.15) - sal)
+        AS "increase"
+FROM emp
+ORDER BY ename ASC, "new salary" ASC
+;
+
+
+
+
+-- End of file
