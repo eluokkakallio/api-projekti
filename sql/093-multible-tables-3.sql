6b92858de7595940f61f1f0c4765738fa1c80a3c
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-02-28 01:36:41 +0200

    add 093

---
+++ sql/093-multible-tables-3.sql
@@ -0,0 +1,20 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-02-28
+-- File: 093-multible-tables-3
+
+SELECT e.ename
+        , d.dname
+        , d.loc
+        , e.comm
+FROM emp AS e
+INNER JOIN dept AS d
+    ON e.deptno = d.deptno
+WHERE e.comm IS NOT NULL
+        AND
+        e.comm <> 0
+ORDER BY e.ename ASC
+;
+
+
+
+-- End of file
