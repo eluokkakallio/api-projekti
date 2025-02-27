37f1d9036254a1cb44230d6479c4eec45a5887a0
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-02-27 03:41:30 +0200

    add 042

---
+++ sql/042-like-2.sql
@@ -0,0 +1,28 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-02-27
+-- File: 042-like-2
+
+SELECT ename
+        , deptno
+        AS "Department"
+FROM emp
+WHERE (LOWER(ename) LIKE '_a%'
+        OR
+        LOWER(ename) LIKE '_e%'
+        OR
+        LOWER(ename) LIKE '_i%'
+        OR
+        LOWER(ename) LIKE '_o%'
+        OR
+        LOWER(ename) LIKE '_u%'
+        OR
+        LOWER(ename) LIKE '_y%')
+        AND
+        (job = 'SALESMAN'
+        OR
+        job = 'ANALYST')
+ORDER BY deptno ASC
+        , ename ASC
+;
+
+-- End of file
