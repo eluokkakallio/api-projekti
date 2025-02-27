d1472a86a67eb23d3f4f6c7a0244b473c397f1e8
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-02-28 00:46:40 +0200

    add 082

---
+++ sql/082-group-functions-2.sql
@@ -0,0 +1,12 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-02-28
+-- File: 081-group-functions-1
+
+SELECT COUNT(*)
+        AS "count of dept 30"
+FROM emp
+WHERE deptno = 30
+;
+
+
+-- End of file
