521af4b83050cd2bb1ee5d2c58dc2d41573400ff
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-02-27 05:11:19 +0200

    add 052

---
+++ sql/052-case-2.sql
@@ -0,0 +1,18 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-02-27
+-- File: 052-case-2
+
+SELECT ename
+        , CASE WHEN deptno = 30
+                    AND
+                    comm IS NULL
+                        THEN 100
+                WHEN deptno = 30
+                    THEN comm * 1.05
+                ELSE comm
+            END
+FROM emp
+;
+
+
+-- End of file
