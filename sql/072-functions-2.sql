a4dcf47663352ff65e2c2a1fb8500a9663e39074
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-02-27 23:07:32 +0200

    add 072

---
+++ sql/072-functions-2.sql
@@ -0,0 +1,14 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-02-27
+-- File: 072-functions-2
+
+SELECT ename
+        AS "Name"
+        , LENGTH(ename)
+        AS "Characters"
+FROM emp
+ORDER BY "Characters" ASC
+;
+
+
+-- End of file
