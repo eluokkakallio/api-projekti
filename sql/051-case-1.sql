be41cb6ff0e0fcd69b68057d023716e69a98dd18
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-02-27 04:41:44 +0200

    add 051

---
+++ sql/051-case-1.sql
@@ -0,0 +1,18 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-02-27
+-- File: 051-case-1
+
+SELECT ename
+        , CASE WHEN job = 'SALESMAN'
+                    THEN 'SALES PERSON'
+                ELSE job
+            END
+        AS "New Job Name"
+FROM emp
+ORDER BY job ASC
+        , ename ASC
+;
+
+
+
+-- End of file
