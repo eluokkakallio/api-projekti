431b1c9eaa6b60a0a2cddaba948f03f02bdf8738
emmi <e.luokkakallio@hotmail.com>
2025-03-17 11:21:11 +0200

    103 add

---
+++ sql/103-multiple-tables-and-self-join-3.sql
@@ -0,0 +1,14 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-02-28
+-- File: 103-multible-tables-and-self-join-3
+
+SELECT employee.ename AS "Employee"
+FROM    emp AS employee
+INNER JOIN emp AS manager
+    ON employee.mgr = manager.empno
+WHERE employee.sal > manager.sal
+ORDER BY employee.ename ASC
+;
+
+
+-- End of file
