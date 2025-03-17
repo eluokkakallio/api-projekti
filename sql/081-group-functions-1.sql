092d7f52875cfc6f23126cd116609ea7ca99b7da
emmi <e.luokkakallio@hotmail.com>
2025-03-17 09:50:48 +0200

    081 update

--- sql/081-group-functions-1.sql
+++ sql/081-group-functions-1.sql
@@ -2,10 +2,10 @@
 -- Date: 2025-02-28
 -- File: 081-group-functions-1
 
-SELECT COUNT(mgr)
+SELECT COUNT(*)
         AS "count of managers"
 FROM emp
-GROUP BY job
+WHERE LOWER(job) IN ('manager', 'president')
 ;
 
 -- End of file
