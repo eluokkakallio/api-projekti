afae4c78afc07662383da1b99f31ae5b6eb08340
emmi <e.luokkakallio@hotmail.com>
2025-03-17 10:25:24 +0200

    094 add

--- sql/094-multible-tables-4.sql
+++ sql/094-multible-tables-4.sql
@@ -2,6 +2,17 @@
 -- Date: 2025-02-28
 -- File: 094-multible-tables-4
 
-
+SELECT emp.ename
+        , dept.dname
+FROM emp
+INNER JOIN dept
+    ON dept.deptno = emp.deptno
+WHERE LOWER(emp.ename) LIKE '%a%'
+        AND
+        emp.sal > 1100
+        AND
+        emp.sal <= 2200
+ORDER BY emp.ename ASC
+;
 
 -- End of file
