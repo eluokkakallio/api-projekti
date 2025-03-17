16bb9d54fa907dff86be2a6b8bfdb829a7615bff
emmi <e.luokkakallio@hotmail.com>
2025-03-17 11:31:03 +0200

    104 add

---
+++ sql/104-multiple-tables-and-self-join-4.sql
@@ -0,0 +1,24 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-02-28
+-- File: 104-multible-tables-and-self-join-4
+
+SELECT dept.loc AS "Location"
+        , dept.dname AS "Department"
+        , employee.ename AS "Employee"
+        , manager.ename AS "Manager"
+
+FROM    emp AS 'employee'
+INNER JOIN emp AS 'manager'
+    ON employee.mgr = manager.empno
+INNER JOIN dept
+    ON employee.deptno = dept.deptno
+INNER JOIN salgrade
+    ON employee.sal BETWEEN salgrade.losal AND salgrade.hisal
+WHERE salgrade.grade > 3
+ORDER BY dept.loc ASC
+        , manager.ename ASC
+        , employee.ename ASC
+;
+
+
+-- End of file
