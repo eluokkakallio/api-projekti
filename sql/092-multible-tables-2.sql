85b2c3f249298ed9fdfa5170a7ef650dc56e07c0
emmi <e.luokkakallio@hotmail.com>
2025-03-17 10:03:18 +0200

    092 ADD

--- sql/092-multible-tables-2.sql
+++ sql/092-multible-tables-2.sql
@@ -2,11 +2,13 @@
 -- Date: 2025-02-28
 -- File: 092-multible-tables-2
 
-SELECT dept.loc
-        , emp.job
+SELECT DISTINCT emp.job
 FROM emp
 INNER JOIN dept
     ON emp.deptno = dept.deptno
+WHERE LOWER(dept.loc) = 'dallas'
+        OR
+        LOWER(dept.loc) = 'new york'
 ORDER BY emp.job ASC
 ;
 -- End of file
