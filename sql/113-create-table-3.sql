67abd53e46627c4880ecad10dedb335b6e1c8478
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-03-18 12:56:42 +0200

    113 add

---
+++ sql/113-create-table-3.sql
@@ -0,0 +1,17 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-03-18
+-- File: 113-create-table-3
+
+CREATE table weather
+(
+    id INTEGER [NOT NULL] [UNIQUE] [PRIMARY KEY]
+    , time_of_reading TIMESTAMP
+    , high DECIMAL
+    , low DECIMAL
+    , sig VARCHAR(2)
+    , comment VARCHAR(200)
+);
+
+--Note: temperatures in format: -10.5 or 30.8
+
+-- End of file
