515431e74fb61218e4ef2cfe2dbf21f9a66a3cb8
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-03-18 13:15:50 +0200

    122 add

---
+++ sql/122-insert-2.sql
@@ -0,0 +1,13 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-03-18
+-- File: 122-insert-2
+
+INSERT INTO football
+(id, first, last, team, comment)
+VALUES
+(1, 'Jim', 'Anders', 'Manchester', 'UPS 10')
+(2, 'Mark', 'Knofles', 'Liverpool', 'Rockwell international')
+(3, 'Jenny', 'Richards', 'Highland', 'Scottish national')
+;
+
+-- End of file
