c24444f0d3c83b2004be0145b7ccbd8653813146
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-03-18 12:40:27 +0200

    111 add

---
+++ sql/111-create-table-1.sql
@@ -0,0 +1,18 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-03-18
+-- File: 111-create-table-1
+
+INSERT INTO football
+(
+    id
+    , first
+    , last
+    , team
+    , comment
+)
+VALUES
+(
+
+);
+
+-- End of file
