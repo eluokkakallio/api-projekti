e3a6b02b6dad7d4e846cd0d84109ef45279e2ed4
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-03-18 13:21:27 +0200

    123 add

---
+++ sql/123-insert-3.sql
@@ -0,0 +1,14 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-03-18
+-- File: 123-insert-3
+
+INSERT INTO weather
+(id, time_of_reading, high, low, sig, comment)
+VALUES
+(1, '2000-12-11 06:00', 5.5, 1.5, 'MK', 'Snow-rain for a while')
+,(2, '2000-12-12 06:00', 4.1, 0.3,'ES',  'Strong wind')
+,(3, '2000-12-14 06:00', 2.0, -5.8, 'RD', 'Night was cold, foggy weather')
+, (4, '2000-12-15 06:00', 12.8, 7.5, 'MK', 'Rainy day')
+;
+
+-- End of file
