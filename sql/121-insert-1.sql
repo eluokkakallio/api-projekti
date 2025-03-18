8caf5ce38068ddeeecc36acae18f0186cf9fc300
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-03-18 13:12:35 +0200

    121 add

---
+++ sql/121-insert-1.sql
@@ -0,0 +1,23 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-03-18
+-- File: 121-insert-1
+
+INSERT INTO person
+(
+    id
+    , last
+    , first
+    , phone
+    , zip
+    , city
+    , address
+)
+VALUES
+(1, 'Doe', 'John', '123455', '11122', 'New York', '2th avenue')
+, (2, 'Jordan Mike', '221122', '01212', 'Washington', 'South Park 3')
+, (3, 'Durak', 'Stephen', '550011', '55654', 'Miami', 'Sea Drive 112')
+;
+
+
+
+-- End of file
