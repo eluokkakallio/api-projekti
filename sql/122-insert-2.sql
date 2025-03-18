80052f74335907d025de5596c2accd8ad6d58e9e
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-03-18 20:13:19 +0200

    update

--- sql/122-insert-2.sql
+++ sql/122-insert-2.sql
@@ -6,8 +6,8 @@ INSERT INTO football
 (id, first, last, team, comment)
 VALUES
 (1, 'Jim', 'Anders', 'Manchester', 'UPS 10')
-(2, 'Mark', 'Knofles', 'Liverpool', 'Rockwell international')
-(3, 'Jenny', 'Richards', 'Highland', 'Scottish national')
+,(2, 'Mark', 'Knofles', 'Liverpool', 'Rockwell international')
+,(3, 'Jenny', 'Richards', 'Highland', 'Scottish national')
 ;
 
 -- End of file
