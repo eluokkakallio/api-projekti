3725d329d3275a309d547953faecf965c4760c3a
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-03-18 20:12:29 +0200

    update

--- sql/121-insert-1.sql
+++ sql/121-insert-1.sql
@@ -14,7 +14,7 @@ INSERT INTO person
 )
 VALUES
 (1, 'Doe', 'John', '123455', '11122', 'New York', '2th avenue')
-, (2, 'Jordan Mike', '221122', '01212', 'Washington', 'South Park 3')
+, (2, 'Jordan', 'Mike', '221122', '01212', 'Washington', 'South Park 3')
 , (3, 'Durak', 'Stephen', '550011', '55654', 'Miami', 'Sea Drive 112')
 ;
 
