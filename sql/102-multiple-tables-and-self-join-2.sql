8f82c755c4c00739c8a9a13ad3fbe7f82479bffe
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-03-18 12:19:49 +0200

    update

--- sql/102-multiple-tables-and-self-join-2.sql
+++ sql/102-multiple-tables-and-self-join-2.sql
@@ -2,15 +2,16 @@
 -- Date: 2025-02-28
 -- File: 102-multible-tables-and-self-join-2
 
-SELECT employee.ename AS "Employee"
-        ,employee.deptno AS "Dept"
-        , colleague.ename AS "Colleagues"
-
+SELECT employee.deptno AS "Dept"
+        ,employee.ename AS "Employee"
+        , colleague.ename AS "Colleague"
 FROM    emp AS employee
 INNER JOIN emp AS colleague
-    ON employee.empno = colleague.empno
+    ON employee.deptno= colleague.deptno
 WHERE employee.deptno = colleague.deptno
-ORDER BY dept DESC,
+        AND
+        employee.empno <> colleague.empno
+ORDER BY dept ASC,
         employee.ename ASC
         , colleague.ename ASC
 ;
