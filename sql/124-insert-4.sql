5b2ad7d0b995c632f75c3420c014a40143573d74
Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
2025-03-18 13:45:39 +0200

    124 add

---
+++ sql/124-insert-4.sql
@@ -0,0 +1,13 @@
+-- Author: Emmi Luokkakallio <emmi.luokkakallio@tuni.fi>
+-- Date: 2025-03-18
+-- File: 124-insert-4
+
+INSERT INTO inventory
+(item, worth, comment)
+VALUES
+('baseball', 150, 'My first baseball')
+, ('bike', 2000, 'For mountain riding')
+, ('playcards', 10, 'Funny pictures')
+;
+
+-- End of file
